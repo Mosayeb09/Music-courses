fa138ac9dedd6e4a44bdad4d73369174a619a0a9
Mosayeb09 <mosayebhossain09@gmail.com>
2024-08-19 18:17:27 +0600

    putting element in Our Courses section

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["cdns-images.dzcdn.net","i1.sndcdn.com","choirlife.com","static.wikia.nocookie.net","pbs.twimg.com","m.media-amazon.com"]
+        domains:["cdns-images.dzcdn.net","i1.sndcdn.com","choirlife.com","static.wikia.nocookie.net","pbs.twimg.com","m.media-amazon.com",'www.libertyparkmusic.com','encrypted-tbn0.gstatic.com','media.licdn.com','m.media-amazon.com','images.unsplash.com']
     }
     
 };
